3f944fb5e3cc42fbffaf06d0061d226a55ce7d89
ArttuLe <lehtonen.arttu@yahoo.fi>
2021-08-20 12:28:31 +0300

    TBB img processing

--- edge_detection.cpp
+++ edge_detection.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <time.h>
+#include <thread>
+#include <stack>
+#include <chrono>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core/utility.hpp>
 
-#ifndef SIMD_OPENCV_ENABLE
-#define SIMD_OPENCV_ENABLE
-#endif
-#include <Simd/SimdLib.hpp>
-
 
 using namespace std;
+using namespace std::chrono;
 
-cv::Mat img,img_resized, left_img,  gray, blurred, edge;
 
 
 // Adjust the CannyEdge threshold values, recommended ratio is 2:1 or 3:1
 int lowerThreshold = 100;
 int maxThreshold = 200;
-int num_frames = 500;
-
-void Canny(int, void*) {
+int num_frames = 200;
 
 
+void image_processing(cv::Mat &input_mat, cv::Mat &output) {
 
+    cv::Mat gray, blurred;
 
+    cvtColor(input_mat, gray, cv::COLOR_BGR2GRAY);
 
     cv::GaussianBlur(gray,
         blurred,
@@ -34,46 +33,60 @@ void Canny(int, void*) {
         3);
 
     cv::Canny(blurred,
-        edge,
-        lowerThreshold,         // lower threshold
-        maxThreshold);           // higher threshold
+        output,
+        lowerThreshold,       
+        maxThreshold);           
 
-    cv::imshow("Edge Detection", edge);
+    cv::imshow("Edge Detection", output);
 }
 
-void simd_imgproc(int, void*){
 
-    typedef Simd::View<Simd::Allocator> View;
-    cv::Mat blur;
-    View image, out, out2;
+class Parallel_process : public cv::ParallelLoopBody
+    {
 
-    image = left_img; //cv::Mat to Simd view
+    private:
+        cv::Mat img;
+        cv::Mat& retVal;
+        int diff;
 
-    Simd::BgrToGray(image, out);
-    Simd::GaussianBlur3x3(out, out2);
+    public:
+        Parallel_process(cv::Mat inputImage, cv::Mat& outImage, int diffVal)
+            : img(inputImage), retVal(outImage), diff(diffVal){}
 
-    blur = out2; //Simd View to cv::Mat
+        virtual void operator()(const cv::Range& range) const
+        {
+            for(int i = range.start; i < range.end; i++)
+            {
+                cv::Mat in(img, cv::Rect(0, (img.rows/diff)*i, img.cols, img.rows/diff));
+                cv::Mat blurred(img, cv::Rect(0, (img.rows/diff)*i, img.cols, img.rows/diff));
+                cv::Mat gray(img, cv::Rect(0, (img.rows/diff)*i, img.cols, img.rows/diff));
 
-    cv::Canny(blur,
-	edge,
-	lowerThreshold,
-	maxThreshold);
+                cv::Mat out(retVal, cv::Rect(0, (retVal.rows/diff)*i, retVal.cols, retVal.rows/diff));
 
-    imshow("Edge Detection", edge);
-}
+                cvtColor(in, gray, cv::COLOR_BGR2GRAY);
+                cv::GaussianBlur(gray,blurred,cv::Size(3,3),3);
+
+                cv::Canny(blurred,out, lowerThreshold, maxThreshold);
+
+
+            }
+        }
+    };
+
 
 
 int main() {
 
     cv::VideoCapture cap;
+    std::stack<cv::Mat> stack; //Allocate memory beforehand to avoid issues with dynamic allocation?
+    cv::Mat img, left_img, gray, out;
 
     int deviceID = 0;
-    int  apiID = cv::CAP_V4L2; //Videocapturing via libv4l
+    int  apiID = cv::CAP_V4L2;    //cv::CAP_V4L2 for Videocapturing via libv4l
+
     cv::setUseOptimized(1);
 
     cap.open(deviceID, apiID);
-
-    //Set image size 720p 2560x720, WVGA 1344x376
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 2560);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
 
@@ -93,37 +106,32 @@ int main() {
 
         cout<< "Optimized code disabled..." << std::endl;
 
+
+
+
     int counter = 0;
-
-
-    time_t start,end;
-
-
-    time(&start);
 
     while(counter < num_frames){
 
         double start = (double)cv::getTickCount();
 
 	    cap >> img;
-
-
-
-        //Zed has 2 cameras, extract them as separate pictures.
+        
+        //Zed has 2 cameras, extract only one of the pictures
         cv::Mat left_img = img(cv::Rect(0, 0, img.cols / 2, img.rows));
+        out = cv::Mat::zeros(left_img.size(), CV_8UC1);
 
+        auto start1 = high_resolution_clock::now();
+        cv::parallel_for_(cv::Range(0, 8), Parallel_process(left_img, out,8));
+        //image_processing(left_img,out);
+        auto stop1 = high_resolution_clock::now();
 
-	//cvtColor(left_img, gray, COLOR_BGR2GRAY);
-	    cv::namedWindow("Edge Detection", cv::WINDOW_AUTOSIZE);
+        auto duration = duration_cast<microseconds>(stop1-start1);
+        auto time = duration.count()/1000;
 
-        // Canny Edge Detector
-	    Canny(0,0);
-
-        //SIMD edge detector
-        //simd_imgproc(0,0);
-
+        cout << "Time taken: "<< time << "ms" << endl; 
+        cv::imshow("Edge Detection", out);
 	    counter++;
-
 
         double end = (double)cv::getTickCount();
 
@@ -132,9 +140,6 @@ int main() {
         if (cv::waitKey(1) >= 0)
              break;
     }
-    time(&end);
-
-    cout << "Average fps over "<< num_frames << " frames: " << (num_frames / (end-start))<<std::endl;
 
 
 
